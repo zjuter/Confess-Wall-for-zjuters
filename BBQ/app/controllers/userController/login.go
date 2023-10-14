95cc8fec949eda6886e1c638d47b82815ae8a6d5
unclezhou <91417040+unclezhou486@users.noreply.github.com>
2023-10-15 01:20:28 +0800

    Update login.go 增加了虚假的token，仅用做演示

--- BBQ/app/controllers/userController/login.go
+++ BBQ/app/controllers/userController/login.go
@@ -1,52 +1,68 @@
 package userController
 
 import (
-	"BBQ/app/models"
-    "BBQ/app/services/userService"
-	"BBQ/app/utils"
-
+	"crypto/rand"
+	"encoding/base64"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
+	"net/http"
 )
 
-type LoginData struct{
+
+//和chatgpt交流许久，更新了一下这部分，创建了一个虚拟的token，因为前端那里需要以token来记录登陆状态。
+//本来想和别的组搞得一样用JWT，但是我只会问chatgpt，搞不来，有点麻烦，总是麻烦，就直接用随机数当token了。
+//仅演示用
+
+type LoginData struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
-func Login(c *gin.Context){
+func Login(c *gin.Context) {
 	//接收参数
 	var data LoginData
 	err := c.ShouldBindJSON(&data)
 	if err != nil {
-		utils.JsonErrorResponse(c, 200501, "参数错误")
+		JsonErrorResponse(c, 200501, "参数错误")
 		return
 	}
 
-	 //判断用户是否存在
-	err = userService.CheckUserExistByUsername(data.Username)
+	// 生成虚假的 token
+	token := generateRandomToken()
+
+	// 返回用户信息及生成的 token
+	JsonSuccessResponse(c, gin.H{
+		"token": token,
+	})
+}
+
+func generateRandomToken() string {
+	// 定义 token 长度（字节数）
+	tokenLength := 32
+
+	// 生成随机字节片段
+	randomBytes := make([]byte, tokenLength)
+	_, err := rand.Read(randomBytes)
 	if err != nil {
-		if err == gorm.ErrRecordNotFound {
-			utils.JsonErrorResponse(c, 1, "账号或密码错误，请重新输入叭！")
-	    }
-		return
-	}
- 
-	 //获取用户信息
-	var user *models.User
-	user, err = userService.GetUserByUsername(data.Username)
-	if err != nil {
-		utils.JsonInternalServerErrorResponse(c)
-		return
+		panic(err)
 	}
 
-	 //判断密码是否正确
-	flag := userService.ComparePwd(data.Password, user.Password)
-	if !flag {
-		utils.JsonErrorResponse(c, 1, "账号或密码错误，请重新输入叭！")
-		return
-	}
-	// 返回用户信息
-	utils.JsonErrorResponse(c, 0, "登录成功")
+	// 将字节片段编码为字符串
+	token := base64.URLEncoding.EncodeToString(randomBytes)
 
+	return token
+}
+
+func JsonSuccessResponse(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusOK, gin.H{
+		"code":    0,
+		"message": "Success",
+		"data":    data,
+	})
+}
+
+func JsonErrorResponse(c *gin.Context, code int, message string) {
+	c.JSON(http.StatusBadRequest, gin.H{
+		"code":    code,
+		"message": message,
+	})
 }
