78eadae9b42086ba153e0ed596a33bbfaa50bdf8
unclezhou <91417040+unclezhou486@users.noreply.github.com>
2023-10-15 17:32:47 +0800

    14号晚上最初的版本

---
+++ app/services/articleService/article.go
@@ -0,0 +1,62 @@
+package articleService
+
+import (
+	"BBQ/app/models"
+	"BBQ/config/database"
+
+	"gorm.io/gorm"
+)
+
+func CheckArticleExistByArticle(ID string) error {
+	result := database.DB.Where("id= ? ", ID).First(&models.Article{})
+	return result.Error
+}
+
+func Article(article models.Article) error {
+	result := database.DB.Create(&article)
+	return result.Error
+}
+
+func UpdateArticle(article models.Article) error {
+	result := database.DB.Save(&article)
+	return result.Error
+}
+
+func CompareCon(pwd1 string, pwd2 string) bool {
+	return pwd1 == pwd2
+}
+
+func GetArticleByID(ID string) (*models.Article, error) {
+	var article models.Article
+	result := database.DB.Where("id = ?", ID).First(&article)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &article, nil
+}
+
+func DeleteArticle(id string) error {
+	result := database.DB.Where("id = ?", id).Delete(&models.Article{})
+	return result.Error
+}
+
+// ////////////////////////////////////////////////////////////////////////
+
+func GetAriticalContext(ID string) ([]models.Article, error) {
+	var articleList []models.Article
+	result := database.DB.Where("id = ?", ID).Find(&articleList)
+	if result.Error != nil && result.Error != gorm.ErrRecordNotFound {
+		return nil, result.Error
+	}
+	return articleList, nil
+}
+
+func GetAriticalList() ([]models.Article, int, error) {
+	var List []models.Article
+	result := database.DB.Find(&List)
+
+	if result.Error != nil && result.Error != gorm.ErrRecordNotFound {
+		return nil, 0, result.Error
+	}
+	return List, int(result.RowsAffected), nil
+}
